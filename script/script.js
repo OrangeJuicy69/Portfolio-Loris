bcef71481d1cee3279b76c14a0d83c3e867a35bd
Orange_juicy <loli.widmer@gmail.com>
2026-02-17 10:22:01 +0100

    passwort geschützt

---
+++ script/script.js
@@ -0,0 +1,44 @@
+// ===== KONFIGURATION =====
+const correctHash = "73b8a7f3cffd4f0a177259cd99937603b61ff45cc20c5beb825ce05b71de1283";
+const redirectPage = "../html/index2.html";
+
+// ===== LOGIN BUTTON FUNKTION =====
+async function checkPassword() {
+  const passwordInput = document.getElementById("password");
+  const errorText = document.getElementById("error");
+
+  const password = passwordInput.value.trim();
+
+  if (!password) {
+    errorText.innerText = "Bitte Passwort eingeben";
+    return;
+  }
+
+  const hash = await sha256(password);
+
+  if (hash === correctHash) {
+    // Login speichern
+    localStorage.setItem("portfolioAuth", "true");
+
+    // Weiterleitung
+    window.location.href = redirectPage;
+  } else {
+    errorText.innerText = "Falsches Passwort";
+  }
+}
+
+// ===== SHA-256 HASH =====
+async function sha256(text) {
+  const data = new TextEncoder().encode(text);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+  return hashArray
+    .map(b => b.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+
+
+// Seite geladen → prüfen
+document.addEventListener("DOMContentLoaded", checkLoginState);
